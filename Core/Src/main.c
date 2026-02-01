8dbd1d0786f60c3b7d23b7196f15734deea4dd14
neilchibber <neilchibberr@gmail.com>
2026-02-01 17:31:03 -0500

    testing!

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,13 +6,14 @@
 #include "../../Platform/Inc/can.h"
 #include "../../Platform/Inc/usart.h"
 
-/* App */
+/* __App__ */
 #include "../../App/Inc/rr_scheduler.h"
 #include "../../App/Inc/can_system.h"
 #include "../../App/Inc/pcb_led_system.h"
-// #include "../../App/Inc/ex_system.h"
+// #include "../../__App__/Inc/ex_system.h"
 #include "../../App/Inc/heartbeat_system.h"
 #include "../../App/Inc/stepper_system.h"
+
 int main(void)
 {
   HAL_Init();
@@ -27,9 +28,17 @@ int main(void)
   RR_AddController(can_system_controller);
   RR_AddController(pcb_led_system_controller);
   RR_AddController(heartbeat_system_controller);
-
   RR_AddController(stepper_system_controller);
   // RR_AddController(ex_system_controller);
+
+  // Simple delay to let system initialize
+  HAL_Delay(100);
+
+  // Spin motor 0 at 500 steps/second in forward direction
+  // Change the port number (0-5) to test different motors
+  // Change the speed (positive = forward, negative = reverse)
+  StepperSystem_SetStepperModel(0, STEPPER_MODEL_NEMA17);  // Set motor model
+  StepperSystem_SetSpeedStepsS(0, 500);  // 500 steps/second forward
 
   while (1)
   {
