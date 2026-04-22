972578311a22d89662640e878440c9149217b0b9
asgar-t <asgartalajawala@gmail.com>
2026-04-22 14:33:07 -0400

    progress ;)

---
+++ App/Inc/l298n_stepper_system.h
@@ -0,0 +1,128 @@
+#ifndef COPY_RENAME_ME_SYSTEM_H
+#define COPY_RENAME_ME_SYSTEM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "stm32l4xx.h"
+
+/*
+ * Rename this file pair and rename the function prefixes below.
+ *
+ * Recommended pattern:
+ *   my_feature_system.h
+ *   my_feature_system.c
+ *
+ *   bool my_feature_system_init(void);
+ *   void my_feature_system_controller(void);
+ */
+
+
+typedef volatile uint32_t* TIMER_CHANNEL;
+typedef	uint16_t GPIO_PIN;
+
+//typedef struct{
+//
+//
+//	TIMER_CHANNEL 	channel;
+//	TIM_TypeDef* 	instance;
+//	GPIO_TypeDef* 	port;
+//	GPIO_PIN 		pin;
+//
+//	uint32_t 		alternate_func;
+//
+//} PWM_CHANNEL;
+
+typedef struct{
+
+	GPIO_TypeDef* 	ena_port;
+	GPIO_PIN 		ena_pin;
+
+	GPIO_TypeDef* 	enb_port;
+	GPIO_PIN 		enb_pin;
+
+	GPIO_TypeDef* 	in1_port;
+	GPIO_PIN 		in1_pin;
+
+	GPIO_TypeDef* 	in2_port;
+	GPIO_PIN 		in2_pin;
+
+	GPIO_TypeDef* 	in3_port;
+	GPIO_PIN 		in3_pin;
+
+	GPIO_TypeDef* 	in4_port;
+	GPIO_PIN 		in4_pin;
+
+
+
+	uint16_t steps_per_rev;
+
+    uint32_t tick_accumulator;
+    uint32_t ticks_per_step;
+	uint16_t current_speed;
+	uint8_t max_speed;
+
+	uint8_t current_step;
+	uint8_t dir;
+
+	uint32_t max_time_ms; //max time
+	uint32_t current_time_running_ms;//how long its been running
+
+	uint8_t status;
+
+}l298n_stepper_driver_t;
+
+
+
+/*
+ *
+ *
+ * PUT CONFIG HERE
+ *
+ *
+ */
+
+#define NUM_STEPPERS 2 //replace w number of steppers
+
+//EXAMPLE
+
+extern l298n_stepper_driver_t drivers[NUM_STEPPERS]; // needs to be defined in the .c
+extern TIM_TypeDef* global_interrupt_clock;
+extern uint32_t l298n_stepper_timer_period_us; //assumes clock speed is 8mhz
+
+
+
+
+/*
+ * Initializes any one-time state for the system.
+ *
+ * Returns:
+ *   true  -> init succeeded
+ *   false -> init failed
+ *
+ * Notes:
+ * - Safe to call once at startup.
+ * - You can also lazy-init from the controller if you prefer.
+ */
+
+/*
+ * Main scheduler callback for this system.
+ *
+ * Add this to main.c with:
+ *   RR_AddController(copy_rename_me_system_controller);
+ *
+ * Notes:
+ * - This should be non-blocking.
+ * - Avoid HAL_Delay() here.
+ * - Use HAL_GetTick() for timed behavior.
+ */
+void l298n_stepper_system_controller(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* COPY_RENAME_ME_SYSTEM_H */
--- App/systems/l298n_stepper_system.c
+++ App/systems/l298n_stepper_system.c
@@ -1,4 +1,4 @@
-#include "copy&renameME_system.h"
+//#include "copy_rename_me_system.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -10,24 +10,14 @@
 #include "can_system.h"
 #include "can_params.h"
 
+
 /*
- * ============================================================================
- *  Rename notes
- * ============================================================================
- *
- * Rename:
- *   copy_rename_me_system_init()
- *   copy_rename_me_system_controller()
- *
- * and the filenames:
- *   copy&renameME_system.h
- *   copy&renameME_system.c
- *
- * Suggested example:
- *   dc_motor_test_system.h
- *   dc_motor_test_system.c
- *   dc_motor_test_system_init()
- *   dc_motor_test_system_controller()
+ * =======================================================================
+ * ASSUMES THE FOLLOWING
+ * 		MAX OF 16 STEPPERS
+ * 		ASSUMES USER DID NOT REPEAT DEFINITIONS FOR PINOUT
+ * 		OTHER ASSUMPTIONS CAN BE DERIVED FROM STRUCTS in .h
+ *========================================================================
  */
 
 /*
@@ -36,31 +26,340 @@
  * ============================================================================
  */
 
-static bool s_initialized = false;
-static uint32_t s_last_tick_ms = 0U;
+#include "l298n_stepper_system.h"
+
+TIM_TypeDef* global_interrupt_clock = TIM2; //EX
+
+/*
+ * ===============================================================
+ * CONFIG
+ * ===============================================================
+ */
+l298n_stepper_driver_t drivers[NUM_STEPPERS]= {};
+
+
+void step_0(l298n_stepper_driver_t* l298n){
+
+
+
+	HAL_GPIO_WritePin(l298n->in1_port,l298n->in1_pin, SET);
+	HAL_GPIO_WritePin(l298n->in2_port,l298n->in2_pin,  RESET);
+
+	HAL_GPIO_WritePin(l298n->in3_port,l298n->in3_pin, SET);
+	HAL_GPIO_WritePin(l298n->in4_port,l298n->in4_pin,  RESET);
+}
+
+void step_1(l298n_stepper_driver_t* l298n){
+
+
+
+	HAL_GPIO_WritePin(l298n->in1_port,l298n->in1_pin, RESET);
+	HAL_GPIO_WritePin(l298n->in2_port,l298n->in2_pin,  SET);
+
+	HAL_GPIO_WritePin(l298n->in3_port,l298n->in3_pin, SET);
+	HAL_GPIO_WritePin(l298n->in4_port,l298n->in4_pin,  RESET);
+}
+
+void step_2(l298n_stepper_driver_t* l298n){
+
+	HAL_GPIO_WritePin(l298n->in1_port,l298n->in1_pin, RESET);
+	HAL_GPIO_WritePin(l298n->in2_port,l298n->in2_pin,  SET);
+
+	HAL_GPIO_WritePin(l298n->in3_port,l298n->in3_pin, RESET);
+	HAL_GPIO_WritePin(l298n->in4_port,l298n->in4_pin,  SET);
+
+}
+
+void step_3(l298n_stepper_driver_t* l298n){
+
+	HAL_GPIO_WritePin(l298n->in1_port,l298n->in1_pin, SET);
+	HAL_GPIO_WritePin(l298n->in2_port,l298n->in2_pin,  RESET);
+
+	HAL_GPIO_WritePin(l298n->in3_port,l298n->in3_pin, RESET);
+	HAL_GPIO_WritePin(l298n->in4_port,l298n->in4_pin,  SET);
+
+
+}
+
+void (*steps[4])(l298n_stepper_driver_t* l298n) = {step_0, step_1, step_2, step_3};
+
+
+
+
+
+static void enable_timer_nvic(TIM_TypeDef* tim) {
+    IRQn_Type irqn;
+
+    if      (tim == TIM1)  irqn = TIM1_UP_TIM16_IRQn;
+    else if (tim == TIM2)  irqn = TIM2_IRQn;
+    else if (tim == TIM3)  irqn = TIM3_IRQn;
+    else if (tim == TIM4)  irqn = TIM4_IRQn;
+    else if (tim == TIM5)  irqn = TIM5_IRQn;
+    else if (tim == TIM6)  irqn = TIM6_DAC_IRQn;
+    else if (tim == TIM7)  irqn = TIM7_IRQn;
+    else return; // unsupported
+
+    NVIC_SetPriority(irqn, 1);
+    NVIC_EnableIRQ(irqn);
+}
+static void enable_timer_clock(TIM_TypeDef* tim) {
+    if      (tim == TIM1)  __HAL_RCC_TIM1_CLK_ENABLE();
+    else if (tim == TIM2)  __HAL_RCC_TIM2_CLK_ENABLE();
+    else if (tim == TIM3)  __HAL_RCC_TIM3_CLK_ENABLE();
+    else if (tim == TIM4)  __HAL_RCC_TIM4_CLK_ENABLE();
+    else if (tim == TIM5)  __HAL_RCC_TIM5_CLK_ENABLE();
+    else if (tim == TIM6)  __HAL_RCC_TIM6_CLK_ENABLE();
+    else if (tim == TIM7)  __HAL_RCC_TIM7_CLK_ENABLE();
+    else return;
+    // etc...
+}
+static void init_global_clk(void){
+
+	enable_timer_clock(global_interrupt_clock);
+	global_interrupt_clock->PSC = 7;
+	global_interrupt_clock->ARR = l298n_stepper_timer_period_us -1;
+    global_interrupt_clock->CR1 |= TIM_CR1_ARPE;
+    global_interrupt_clock->DIER |= TIM_DIER_UIE;
+    global_interrupt_clock->SR &= ~TIM_SR_UIF;
+    global_interrupt_clock->CR1 |= TIM_CR1_CEN;
+
+
+    enable_timer_nvic(global_interrupt_clock);
+
+}
+static bool l298n_stepper_system_init(void);
+
+static bool init_l298n(l298n_stepper_driver_t*);
+static bool init_gpio(l298n_stepper_driver_t*);
+
+
+#define L298N_STEPPER_STATUS_UNDEFINED 		(uint8_t)0
+#define L298N_STEPPER_STATUS_IDLE 			(uint8_t)1
+#define L298N_STEPPER_STATUS_STARTUP		(uint8_t)2
+#define L298N_STEPPER_STATUS_ERR_INVREQ		(uint8_t)3
+#define L298N_STEPPER_STATUS_ERR_DISARM 	(uint8_t)4
+#define L298N_STEPPER_STATUS_ERR_FAIL 		(uint8_t)5
+#define L298N_STEPPER_STATUS_ERR_CONTRFAIL	(uint8_t)6 	//probably unused
+#define L298N_STEPPER_STATUS_ERR_ESTOP		(uint8_t)7	//probably unused
+#define L298N_STEPPER_STATUS_ERR_POS_UNKOWN (uint8_t)8
+#define L298N_STEPPER_STATUS_POS_CONTR		(uint8_t)9
+#define L298N_STEPPER_STATUS_VEL_CONTR		(uint8_t)10
+#define L298N_STEPPER_STATUS_STOPPED		(uint8_t)11
+
+
+void do_step(l298n_stepper_driver_t* l298n){
+
+
+	l298n->current_step += l298n->dir;
+	l298n->current_step &= 0b011;
+	steps[l298n->current_step](l298n);
+
+
+
+}
+
+void stepper_tick(l298n_stepper_driver_t* stepper) {
+    stepper->tick_accumulator++;
+    if (stepper->tick_accumulator >= stepper->ticks_per_step) {
+        stepper->tick_accumulator = 0;
+        do_step(stepper);
+    }
+}
+
+static void stepper_irq_handler(void) {
+    if (global_interrupt_clock->SR & TIM_SR_UIF) {
+        global_interrupt_clock->SR &= ~TIM_SR_UIF;
+        for (int i = 0; i < NUM_STEPPERS; i++) {
+            stepper_tick(&drivers[i]);
+        }
+    }
+}
+
+__weak void TIM1_UP_TIM16_IRQHandler(void) { stepper_irq_handler(); }
+__weak void TIM2_IRQHandler(void)          { stepper_irq_handler(); }
+__weak void TIM3_IRQHandler(void)          { stepper_irq_handler(); }
+__weak void TIM4_IRQHandler(void)          { stepper_irq_handler(); }
+__weak void TIM5_IRQHandler(void)          { stepper_irq_handler(); }
+__weak void TIM6_DAC_IRQHandler(void)      { stepper_irq_handler(); }
+__weak void TIM7_IRQHandler(void)          { stepper_irq_handler(); }
+
 
 /*
  * ============================================================================
  *  Init
  * ============================================================================
  */
+static bool l298n_stepper_system_init(void){
 
-bool copy_rename_me_system_init(void)
-{
-  /*
-   * Put one-time setup here.
-   *
-   * Examples:
-   * - initialize local state
-   * - initialize platform peripherals used only by this system
-   * - write default CAN parameter values
-   */
 
-  s_initialized = true;
-  s_last_tick_ms = HAL_GetTick();
+	for(int i = 0; i < NUM_STEPPERS; i++){
 
-  return true;
+		if(!init_l298n(drivers + i)) return false;
+	}
+
+	init_global_clk();
+	return true;
+
 }
+
+static bool init_l298n(l298n_stepper_driver_t* l298n){
+
+
+
+	if (!init_gpio(l298n)) return false;
+	//if (!init_pwm(l298n)) return false;
+
+	return true;
+
+}
+
+static bool init_gpio(l298n_stepper_driver_t*l298n){
+
+	//constants used, help with iteration
+	const int num_ports = 8;
+	const uint32_t port_dist = ((uint32_t)GPIOB-(uint32_t)GPIOA);
+
+
+	//put all the GPIOs that will be used in an array for iteration
+	GPIO_TypeDef* ports[6] = {l298n->ena_port,l298n->enb_port,l298n->in1_port,l298n->in2_port,l298n->in3_port,l298n->in4_port};
+	GPIO_PIN pins[6] = {l298n->ena_pin,l298n->enb_pin,l298n->in1_pin,l298n->in2_pin,l298n->in3_pin,l298n->in4_pin};
+
+
+	//tracks ports we've seen, and the pins needed for each port
+	bool ports_seen[num_ports] = {};
+	GPIO_PIN pins_for_port[num_ports] = {};
+
+	//iterate through all the ports we will use, set seen flag and |= the pins
+	uint8_t index = 0;
+	for (int i = 0; i < 6; i++){
+
+		//index offset, verified constant address difference between the port base addr (port_dist)
+		index = ((uint32_t)ports[i] - (uint32_t)GPIOA)/port_dist;
+
+		if (index > num_ports-1) return false;
+
+		//set flag
+		ports_seen[index] = true;
+		pins_for_port[index] |= pins[i];
+
+	}
+
+
+
+	//start initialize
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	//iterate through all ports
+	for (int i = 0; i< num_ports; i++){
+
+		//if we need to initialize the port
+		if(ports_seen[i]){
+
+			//case statement unavoidable - the RCC enables are macro'd, so can't do function pointer array :(
+			switch(i){
+			 	 case 0: __HAL_RCC_GPIOA_CLK_ENABLE(); break;
+			 	 case 1: __HAL_RCC_GPIOB_CLK_ENABLE(); break;
+			 	 case 2: __HAL_RCC_GPIOC_CLK_ENABLE(); break;
+			 	 case 3: __HAL_RCC_GPIOD_CLK_ENABLE(); break;
+			 	 case 4: __HAL_RCC_GPIOE_CLK_ENABLE(); break;
+			 	 case 5: __HAL_RCC_GPIOF_CLK_ENABLE(); break;
+			 	 case 6: __HAL_RCC_GPIOG_CLK_ENABLE(); break;
+			 	 case 7: __HAL_RCC_GPIOH_CLK_ENABLE(); break;
+
+			}
+
+		//initialize the port and pin
+		/*
+		 * OFFSET MATH: convert GPIO (index 0) to uint32_t to avoid pointer arithmetic
+		 * 				add index*offset
+		 * 				done
+		 *
+		 * 				Pointer arithmetic needs to be avoided because the GPIO_TypeDef is
+		 * 				much smaller than the actual memory dedicated for each GPIO port (1KB = 0x0400)
+		 */
+
+		HAL_GPIO_WritePin((GPIO_TypeDef*)((uint32_t)GPIOA + i*port_dist), pins_for_port[i], GPIO_PIN_RESET);
+		GPIO_InitStruct.Pin = pins_for_port[i];
+		GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+		GPIO_InitStruct.Pull = GPIO_PULLUP;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		HAL_GPIO_Init((GPIO_TypeDef*)((uint32_t)GPIOA + i*port_dist), &GPIO_InitStruct);
+
+
+		 }
+
+	 }
+
+	 return true;
+
+
+}
+
+/*
+ * We get speed in degrees/sec
+ * we have X steps/rev
+ * We have sec/tick
+ * we need ticks/step
+ *
+ * (d/s) *1/360(rev/d)*(steps/rev)*(us/tick)*1/1000000(s/us) ^-1
+ *1/(us/tick)*1000000(us/s)*
+ *
+ *
+ */
+
+static bool update_speed(l298n_stepper_driver_t* l298n, int16_t speed){
+
+	if (speed < 0){
+		if (-1*speed > l298n->max_speed) return false;
+		l298n->dir = -1;
+		speed = -1*speed;
+	}
+	else if(speed > 0){
+		if (speed > l298n->max_speed) return false;
+		l298n->dir = 1;
+	}
+	else{
+		l298n->dir = 0; l298n->ticks_per_step = (1<<31); return true;
+	}
+
+	l298n->ticks_per_step = (uint32_t)(
+	    (1000000.0f / l298n_stepper_timer_period_us) /
+	    ((float)speed / 360.0f * l298n->steps_per_rev));
+	return true;
+}
+
+
+bool is_init = 0;
+void l298n_stepper_system(void){
+	if (!is_init){l298n_stepper_system_init; is_init = true;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
 
 /*
  * ============================================================================
@@ -68,31 +367,31 @@ bool copy_rename_me_system_init(void)
  * ============================================================================
  */
 
-void copy_rename_me_system_controller(void)
-{
-  /*
-   * Lazy init pattern:
-   * If you want the system to self-start when first scheduled, keep this.
-   * Otherwise you can call init from main.c and remove this block.
-   */
-  if (!s_initialized)
-  {
-    if (!copy_rename_me_system_init())
-    {
-      return;
-    }
-  }
+//void copy_rename_me_system_controller(void)
+//{
+//  /*
+//   * Lazy init pattern:
+//   * If you want the system to self-start when first scheduled, keep this.
+//   * Otherwise you can call init from main.c and remove this block.
+//   */
+//  if (!s_initialized)
+//  {
+//    if (!copy_rename_me_system_init())
+//    {
+//      return;
+//    }
+//  }
 
   /*
    * Example non-blocking periodic logic.
    * Runs every 100 ms.
    */
-  uint32_t now = HAL_GetTick();
-  if ((now - s_last_tick_ms) < 100U)
-  {
-    return;
-  }
-  s_last_tick_ms = now;
+//  uint32_t now = HAL_GetTick();
+//  if ((now - s_last_tick_ms) < 100U)
+//  {
+//    return;
+//  }
+//  s_last_tick_ms = now;
 
   /*
    * Put your real recurring logic here.
@@ -112,7 +411,7 @@ void copy_rename_me_system_controller(void)
    *   // do something with value
    * }
    */
-}
+//}
 
 /*
  * ============================================================================
